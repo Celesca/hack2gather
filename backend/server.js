102d8fcaf07205fa41628e6d1b2b88e65d4942d8
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-09-25 14:06:30 +0700

    add: mysql

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const app = express();
 const port = 3000;
-
 const cors = require('cors');
+const mysql = require('mysql2');
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'mydb'
+});
+
+const app = express();
+app.use(express.json())
 app.use(cors());
 
 app.get('/v1/healthcheck', (req, res) => {
