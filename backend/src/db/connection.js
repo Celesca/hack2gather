1fe7ae75b8d55d5070ffca96eabe75dc2709fce6
celesca <folk.sawit@gmail.com>
2024-10-17 11:25:50 +0700

    add: try-catch

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -1,4 +1,12 @@
 const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
+
+let prisma
+
+try {
+    prisma = new PrismaClient()
+} catch (error) {
+    console.error('Failed to initialize Prisma Client:', error)
+    process.exit(1) // Exit the process with a failure code
+}
 
 module.exports = prisma
